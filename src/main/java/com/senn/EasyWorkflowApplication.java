411da0fa53e64e952beae997f9e745a9870b6fbb
jiangsensen <64098056@qq.com>
2024-04-08 20:00:55 +0800

    init

---
+++ src/main/java/com/senn/EasyWorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.senn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyWorkflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyWorkflowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/senn/EasyWorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyWorkflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
